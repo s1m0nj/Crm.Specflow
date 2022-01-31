7d909a9b66a2f72c3c295949a0ed4c04f1fa65da
Martijn Vermaat <mvermaat@hso.com>
2022-01-31 13:56:29 +0100

    temp

---
+++ Vermaat.Crm.Specflow/Steps/GivenAnyRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Vermaat.Crm.Specflow.Steps
+{
+    [Binding]
+    public class GivenAnyRecord
+    {
+        [Given(@"Given a '(.*)' named ([^\s]+) with the following parameters")]
+        public void GivenAnyRecordWithParams(string record, string alias, Table parameters)
+        {
+
+        }
+    }
+}
